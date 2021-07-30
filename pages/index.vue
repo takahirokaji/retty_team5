fa4d3414f4d04b54250605be5eb41235e2497c84
takahirokaji <tk69kj619@gmail.com>
2021-07-30 16:53:06 +0900

    I found target restaurant

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <input type="file" accept=".csv" @change="fileHandler" />
+      <button @click=""></button>
     </v-col>
   </v-row>
 </template>
@@ -32,11 +33,37 @@ export default {
             onlyYakiniku.push(el)
           }
         });
+        const only5 = [];
+        onlyYakiniku.map(el => {
+          if(el[4] <= '17:00:00') {
+            only5.push(el)
+          }
+        })
+        only5.map(el => {
+          let tokyoLat = 139.7369874;// ある駅の緯度
+          let tokyoLng = 35.6546783;
+          if(this.distance(tokyoLat, tokyoLng, el[2], el[1]) < 1) {
+            console.log(el[0])
+          };
+        })
       };
       reader.onerror = function () {
         alert("エラー：ファイルをロードできません。");
       };
     },
+    distance(lat1, lng1, lat2, lng2) {
+  lat1 *= Math.PI / 180;
+  lng1 *= Math.PI / 180;
+  lat2 *= Math.PI / 180;
+  lng2 *= Math.PI / 180;
+  return (
+    6371 *
+    Math.acos(
+      Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) +
+        Math.sin(lat1) * Math.sin(lat2)
+    )
+  );
+}
   },
 
 }
