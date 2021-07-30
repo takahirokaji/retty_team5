47cad52ac70b1d8d8c36063e5b6a6581b529a4f2
takahirokaji <tk69kj619@gmail.com>
2021-07-30 14:34:38 +0900

    change

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,33 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <input type="file" accept=".csv" @change="fileHandler" />
     </v-col>
   </v-row>
 </template>
+
+<script lang="js">
+export default {
+  data() {
+    return {
+      file: '',
+    }
+  },
+  methods: {
+    fileHandler(e) {
+      const target = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsText(target);
+
+      reader.onload = async(e) => {
+        const result = e.target.result;
+        console.log(await result)
+      };
+      reader.onerror = function () {
+        alert("エラー：ファイルをロードできません。");
+      };
+    }
+  },
+
+}
+</script>
